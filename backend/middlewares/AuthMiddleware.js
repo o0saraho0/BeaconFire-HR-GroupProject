f9205750cb21b85efd6bd613c691e0cce4a3fdd0
Kewei Zhang <jeffzhkw@gmail.com>
2025-01-13 18:14:07 -0500

    Added 2 new middlewares for role checking

--- backend/middlewares/AuthMiddleware.js
+++ backend/middlewares/AuthMiddleware.js
@@ -3,7 +3,11 @@ This file should contain middleware that checks if req contains JWT in the heade
 */
 import validator from 'validator';
 import jwt from 'jsonwebtoken';
+import HRProfile from "../models/HRProfile.js"
+import EmployeeProfile from "../models/EmployeeProfile.js"
 
+
+// check if it's logged in, for protecting endpoints that requires JWT.
 export const jwtValidation = (req, res, next) => {
 
 
@@ -46,3 +50,32 @@ export const jwtValidation = (req, res, next) => {
         return res.status(500).json({ message: err.message });
     }
 };
+
+// for protecting endpoints that only HR can have access to 
+export const authenticatedHR = async (req, res, next) => {
+    const user_id = req.body.user_id
+
+    const hr = await HRProfile.findOne({ user_id })
+
+    if (hr) {
+        next()
+
+    }
+    return res.status(403).json({ message: "You are not a HR" })
+
+
+}
+
+// Check if the logged in user is Employee, for protecting endpoints that only employee can have access to 
+export const authenticatedEmployee = async (req, res, next) => {
+
+    const user_id = req.body.user_id
+
+    const employee = await EmployeeProfile.findOne({ user_id })
+
+    if (employee) {
+        next()
+    }
+    return res.status(403).json({ message: "You are not a Employee" })
+
+}
