887ce2a3aff4b28dff6d691a9b125ddc92892e8d
o0saraho0 <sarah.jiang.1229@gmail.com>
2025-01-13 22:45:22 -0800

    initial setup for angular

--- frontend/hr-angular/src/app/app-routing.module.ts
+++ frontend/hr-angular/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeeProfilesComponent } from './components/employee-profiles/employee-profiles.component';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { VisaStatusManagementComponent } from './components/visa-status-management/visa-status-management.component';
+import { HiringManagementComponent } from './components/hiring-management/hiring-management.component';
+import { HousingManagementComponent } from './components/housing-management/housing-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'employee-profiles', component: EmployeeProfilesComponent, children: [
+    {
+      path: ':id', 
+      component: EmployeeDetailComponent
+    }
+  ]},
+  { path: 'visa-status', component: VisaStatusManagementComponent },
+  {path: 'hiring', component: HiringManagementComponent},
+  { path: 'housing', component: HousingManagementComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/hr-angular/src/app/app.module.ts
+++ frontend/hr-angular/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeeProfilesComponent } from './components/employee-profiles/employee-profiles.component';
+import { VisaStatusManagementComponent } from './components/visa-status-management/visa-status-management.component';
+import { HiringManagementComponent } from './components/hiring-management/hiring-management.component';
+import { HousingManagementComponent } from './components/housing-management/housing-management.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { FormControl } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeeProfilesComponent,
+    VisaStatusManagementComponent,
+    HiringManagementComponent,
+    HousingManagementComponent,
+    NavbarComponent,
+    LoginComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
