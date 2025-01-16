9bb57f4125a2021bfb78f11872f465fea2e721cf
o0saraho0 <sarah.jiang.1229@gmail.com>
2025-01-15 20:50:55 -0800

    navbar done with materialUI, links need to be updated

--- frontend/employee-react/src/components/Navbar/Navbar.jsx
+++ frontend/employee-react/src/components/Navbar/Navbar.jsx
@@ -1,38 +1,71 @@
+import { useState, useEffect } from "react";
 import { Tabs, Tab, AppBar, Box, Button } from "@mui/material";
-import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [value, setValue] = useState(0);
+
+  // Synchronize the `value` with the current URL path
+  useEffect(() => {
+    switch (window.location.pathname) {
+      case "/personalprofile":
+        setValue(0);
+        break;
+      case "/visa-status":
+        setValue(1);
+        break;
+      case "/housing":
+        setValue(2);
+        break;
+      default:
+        setValue(0);
+    }
+  }, []);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    switch (newValue) {
+      case 0:
+        window.location.href = "/personalprofile";
+        break;
+      case 1:
+        window.location.href = "/visa-status";
+        break;
+      case 2:
+        window.location.href = "/housing";
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <Router>
-      <AppBar position="fixed" color="primary">
-        <Box sx={{ display: "flex", alignItems: "center", padding: 2 }}>
-          <Tabs
-            textColor="inherit"
-            indicatorColor="secondary"
-            sx={{ flexGrow: 2 }}
-          >
-            <Tab
-              label="Personal Information"
-              component={Link}
-              to="/personalprofile"
-            />
-            <Tab
-              label="Visa Status Management"
-              component={Link}
-              // to="/visa-status"
-            />
-            <Tab
-              label="Housing"
-              component={Link}
-              // to="/housing"
-            />
-          </Tabs>
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
-        </Box>
-      </AppBar>
-    </Router>
+    <AppBar position="fixed" color="primary">
+      <Box sx={{ display: "flex", alignItems: "center", padding: 2 }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="inherit"
+          indicatorColor="secondary"
+          sx={{
+            flexGrow: 2,
+            "& .MuiTabs-indicator": {
+              backgroundColor: "white",
+            },
+          }}
+        >
+          <Tab label="Personal Information" />
+          <Tab label="Visa Status Management" />
+          <Tab label="Housing" />
+        </Tabs>
+        <Button
+          color="inherit"
+          href="/login"
+          sx={{ textDecoration: "none", color: "inherit" }}
+        >
+          Login
+        </Button>
+      </Box>
+    </AppBar>
   );
 };
 
