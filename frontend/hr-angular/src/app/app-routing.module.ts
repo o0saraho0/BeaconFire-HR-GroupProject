870f8883a070fbc6adfa99f3f16749b9ac8fbcb2
willyao666 <13605529+willyao666@user.noreply.gitee.com>
2025-01-19 15:41:01 -0600

    update

--- frontend/hr-angular/src/app/app-routing.module.ts
+++ frontend/hr-angular/src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
   {
     path: 'visa-status',
     component: VisaStatusManagementComponent,
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'hiring',
